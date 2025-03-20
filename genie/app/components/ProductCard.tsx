12626b3ec71bf183906721f988a546569f416e07
Kevin <digitalkdogg@gmail.com>
2025-03-19 19:51:35 -0500

    adding genie back in

---
+++ genie/app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react"
+import AddToCard from "./AddToCard";
+import styles from './ProductCard.module.css';
+
+const ProductCard = () => {
+  return (
+    <div className={styles.card}>
+       <AddToCard />
+    </div>
+  )
+};
+
+export default ProductCard;
